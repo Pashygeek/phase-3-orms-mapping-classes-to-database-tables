aeda29e95a018c2b1ee1f38a644507c9b66e2cd7
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-06-01 12:14:50 +0300

    Completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -9,30 +9,33 @@ class Song
   end
 
   def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      album TEXT
-    )
-    SQL
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
     DB[:conn].execute(sql)
   end
 
   def save
     sql = <<-SQL
-    INSERT INTO songs (name, album)
-    VALUES (?, ?)
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
     SQL
+
     DB[:conn].execute(sql, self.name, self.album)
-  end
 
-  def self.create(name:, album:)
-    song= Song.new(name: name, album: album)
-    song.save
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    # return the Ruby instance
+    self
+
   end
+  
+ def self.create(name:, album:)
+  song = Song.new(name: name, album: album)
+  song.save
 end
-
-Song.create_table
-song = Song.create(name: "Hello", album: "25")
-puts song.name
+end
